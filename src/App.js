bed31a1ce14ca0ac0f1461a84cc769cc6f3ce95e
Hyeyeon Shin <hyeyeonshin@DF-586065.local>
2018-09-29 12:50:43 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Other from './scenes/otherpage/otherpage'
 import Hints from './scenes/hints/hints'
 import Section from './components/section'
 import './App.css'
+import Logo from './logo.png'
+
 
 class App extends Component {
   render() {
     return (
+      <div className="background">
+      <img src={Logo} className="logo"/>
+      <div className="info">
+        <div className="time">10:30</div>
+        <div className="date">September 29, 2018</div>
+        <div className="name">Ladies Learning Code</div>
+        <div className="classNumber">Workshop 3</div>
+     </div>
       <HashRouter>
         <Switch>
           <Route exact path="/" component={Home} />
@@ -18,6 +28,7 @@ class App extends Component {
 
         </Switch>
       </HashRouter>
+      </div>
     )
   }
 }
