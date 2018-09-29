5775115834090876241ee5ec5d988cf46c355b0e
Hyeyeon Shin <hyeyeonshin@DF-586065.local>
2018-09-29 02:31:04 -0400

    changes

--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import NavBar from '../../components/navigation/NavBar';
 import code from './code.png'
+import output from './output.png'
 import './home.css'
 
 class Home extends Component {
@@ -10,6 +11,7 @@ class Home extends Component {
       <div className="container">
       <img className="code" src={code}/>
       <div className="hintMessage">error get a </div>
+      <img className="output" src={output}/>
     </div></div>)
     
   }
